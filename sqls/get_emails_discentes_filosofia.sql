d4d1189679b5e3cefc9b9d58c10054792dfca00b
Arthur Ferreira <arthurferreira@usp.br>
2020-03-26 20:44:12 -0300

    Consulta e-mails dos discentes da Graduação, separados por curso.

---
+++ sqls/get_emails_discentes_filosofia.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT l.codema FROM LOCALIZAPESSOA l
+    JOIN SITALUNOATIVOGR s
+    ON s.codpes = l.codpes 
+    WHERE l.tipvin = 'ALUNOGR' 
+        AND l.codundclg = 8 
+        AND s.codcur = 8010
