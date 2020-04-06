f2ee4a57aabbacad9674d13da2009f82e4bf9151
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2020-04-06 15:34:46 -0300

    fix utils

--- app/Utils.php
+++ app/Utils.php
@@ -4,7 +4,8 @@ namespace App;
 
 class Utils {
 
-    public static function trimEmails(string $emails){
+    public static function trimEmails($emails){
+        if (empty(trim($emails))) return '';
         $emails_array = array_map('trim', explode(',', $emails));
         sort($emails_array);
         return implode(',',$emails_array);
