b7d4d28cd21b18b4a741763339c84ae00eae30f8
Victor Oliveira <vitor.oliveira.marinho@gmail.com>
2021-09-20 14:12:10 -0300

    atualizacao senhaunica

--- config/listas.php
+++ config/listas.php
@@ -4,7 +4,7 @@ FFLCH | Faculdade de Filosofia, Letras e Ciências Humanas
 ";
 
 return [
-    'admins' => env('ADMINS'),
+    'admins' => env('SENHAUNICA_ADMINS'),
     'mailman_domain' => env('MAILMAN_SUFFIX','@listas.usp.br'),
     'mailman_owner' => env('MAILMAN_OWNER','fflchsti@usp.br'),
     'mailman_footer' => utf8_decode(env('MAILMAN_FOOTER',$footer)),
